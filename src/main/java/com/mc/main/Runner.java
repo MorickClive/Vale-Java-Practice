b1df5c81dc8f98f24dc6648a46ca8eb6216b5acb
Alex Davies <MorickClive@Gmail.com>
2022-02-21 19:29:42 +0000

    AD: unlocking Runner app up to and including OOP

--- src/main/java/com/mc/main/Runner.java
+++ src/main/java/com/mc/main/Runner.java
@@ -27,8 +27,8 @@ public class Runner {
 	protected static void start() {
 		System.out.println("=".repeat(40) + "\n");
 		
-		//Application.PROCEDURAL.run();
-		//Application.OOP.run();
+		Application.PROCEDURAL.run();
+		Application.OOP.run();
 		Application.ADVANCED.run();
 		//Application.JAVA8.run();
 	}
